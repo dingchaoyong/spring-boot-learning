1e07aa8eed5b3e0462994f2e9ea4d75d48aefa32
dingchaoyong <15828307054@163.com>
2015-03-16 13:13:55 +0800

    start web application

---
+++ dev/src/main/java/com/yuqian/run/Application.java
@@ -0,0 +1,16 @@
+package com.yuqian.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * the main applicationContext
+ * 
+ * @author dcy created at 2015年3月15日
+ *
+ */
+@SpringBootApplication
+public class Application {
+    public static void main( String[] args ) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ dev/src/main/java/com/yuqian/run/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.yuqian.run.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${app.description}")
+    private String name;
+    @RequestMapping("/")
+    public String sayHello() {
+        return String.format("%s", name);
+    }
+}
